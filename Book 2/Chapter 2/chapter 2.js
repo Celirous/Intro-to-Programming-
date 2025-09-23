dc32685449be8d4fbb04c726b4dd1d6985a3b7b6
Celirous <aj.bellringer@gmail.com>
2025-09-23 12:59:13 +0200

    Add files via upload

---
+++ Book 2/Chapter 2/chapter 2.js
@@ -0,0 +1,22 @@
+
+
+alert("Coding is difficult but fun!");
+
+
+for (var i = 0; i < 10; i++) {
+  document.write("Coding is fun!<br>");
+}
+
+
+for (var j = 0; j < 15; j++) {
+  console.log("Hey dude, eat the code now ");
+};
+
+
+for (var k = 0; k < 12; k++) {
+    document.write("Joe said, \"Hi\"<br>"); 
+}; 
+
+for (var l = 0; l < 4; l++) {
+    document.write("Joe said, 'Hi'<br>");
+};
