a82ef8e931bd5c57e1e5909c89efccea9d1953b9
Celirous <aj.bellringer@gmail.com>
2025-10-02 10:58:17 +0200

    Add files via upload

---
+++ Book 2/Chapter 12/chapter 12.js
@@ -0,0 +1,24 @@
+// Basic function that will return a var we set 
+
+function smileyIt(theText) {
+    theText += " :) ";
+    return theText;
+};
+
+smileyIt("Hi there!");
+
+console.log(smileyIt("Hi there!"))
+
+
+//  this is basic math being done with function
+function whatsTheNumber() {
+    return 3000;
+};
+console.log(whatsTheNumber());
+console.log(whatsTheNumber() + 80);
+
+var theTotal = whatsTheNumber() + 80; 
+console.log(theTotal);
+
+
+
